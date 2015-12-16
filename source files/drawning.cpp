9a82ef866ca34e7b2108c90fb47d7cd4e403a437
Kamil Musialski <kmusialski@albatros-aluminium.com>
2015-12-16 14:51:53 +0100

    paradox 0.3  Please enter the commit message for your changes. Lines starting

--- source files/drawning.cpp
+++ source files/drawning.cpp
@@ -1,42 +1,42 @@
 #include "Drawning.h"
-int counter = 0;
 
 
-void counting(int iLiczba, int tab[], int ile )
+void counting(int iLiczba, int tab[], int ile,int& counter )
 {
 
    //if( ile <= 0 ) // ile liczb do losowania, jezeli sie skoncza to false
     int i = 0;
-
     do
     {
-        if( tab[ i ] == iLiczba ){
-             counter = counter++;
-
+        if( tab[ i ] == iLiczba ){ // sprawdzenie czy dany element juz wystapil
+            counter++;
+            tab[i] = NULL; // zastapienie powtorzenoego elemetu zerowyum znakiem
         }
         i++;
     } while( i < ile );
 }
 
+
 int wylosuj()
 {
-    return( rand() % 8 ) + 1;
+    return( rand() % 365 ) + 1;
 }
 
 int DRAW::cinInt()
 {
-
+    int countPercent;
+    int trials = 20;
     srand( time( NULL ) );
-    for (int i =0; i < 1; i++){
-    int elements = 8;
+    for (int i =0; i < trials; i++){
+    int elements = 25;
     int wylosowane[ elements ];
     int wylosowanych = 0;
-
+    int counter = 0;
     do
     {
         int liczba = wylosuj();
-        counting(liczba,wylosowane,wylosowanych);
-      //  counting( licznik,liczba, wylosowane, wylosowanych ) ;
+     //   cout<<liczba<<" ";
+        counting(liczba,wylosowane,wylosowanych,counter);
 
             wylosowane[ wylosowanych ] = liczba;
             wylosowanych++;
@@ -44,17 +44,28 @@ int DRAW::cinInt()
     } while( wylosowanych < elements );
 
     wylosowanych = 0;
-   do
-   {
-      cout << wylosowane[ wylosowanych ]<<" ";
-       wylosowanych++;
-       cout<<" ";
-   } while( wylosowanych < elements );
+ //  do
+  // {
+   //   cout << wylosowane[ wylosowanych ]<<" ";
+    //   wylosowanych++;
+   //    cout<<" ";
+  // } while( wylosowanych < elements );
 
     cout<<endl;
     cout<<"ilosc powtorzen"<<endl;
-    cout<<counter<<endl;
+    percent(countPercent,counter,trials);
     }
 
     return 0;
     }
+
+void percent(int& countPercent,int& counter, int& trials){
+
+if (counter > 0){
+    countPercent++;
+}
+if
+cout<<countPercent/trials
+
+}
+
